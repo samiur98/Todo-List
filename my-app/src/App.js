1c7bcad2f84fa8785fd28f2e12ca10b3b5ffda2b
samiur98 <sam@Shahs-MacBook-Pro.local>
2020-06-28 17:35:42 -0700

    Checked functionality added and Textarea added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -6,7 +6,7 @@ class App extends React.Component{
   constructor() {
     super();
     this.state = {
-      rabbit: 'rabbit',
+      idCounter: 0,
       items: [
         {
         id: 1,
@@ -16,20 +16,62 @@ class App extends React.Component{
         {
           id: 2,
           text: 'Sam smith is amazing',
-          checked: false
+          checked: true
         }
       ]
     };
     this.renderTodoItems = this.renderTodoItems.bind(this);
+    this.renderTextArea = this.renderTextArea.bind(this);
     this.renderAddButton = this.renderAddButton.bind(this);
+    this.onCheck = this.onCheck.bind(this);
   }
   
+  getItemStyle(checked) {
+    if(checked) {
+      return {
+        color: 'tomato',
+        textDecoration: 'line-through'
+      };
+    } else {
+      return {};
+    }
+  }
+
+  onCheck(id) {
+    const newItems = [];
+
+    for(let i = 0; i < this.state.items.length; i++) {
+      if(this.state.items[i].id === id) {
+        const newItem = {
+          id: this.state.items[i].id,
+          text: this.state.items[i].text,
+          checked: !this.state.items[i].checked
+        }
+        newItems.push(newItem);
+      } else {
+        newItems.push(this.state.items[i]);
+      }      
+    }
+
+    this.setState(prevState => {
+      return {
+        idCounter: prevState.idCounter, 
+        items: newItems
+      }
+    });
+  }
+
   renderTodoItems() {
     const itemComponents = this.state.items.map(item => {
+      const itemStyle = this.getItemStyle(item.checked);
       return(
         <div className='item'>
-          <input type='checkbox' />
-          <h2>{ item.text }</h2>
+          <input 
+          type='checkbox'
+          checked = { item.checked }
+          onChange = { () => this.onCheck(item.id) } 
+          />
+          <h2 style={ itemStyle }>{ item.text }</h2>
           <button>DEL</button>
         </div>
       );
@@ -41,6 +83,14 @@ class App extends React.Component{
     );
   }
   
+  renderTextArea() {
+    return(
+      <div>
+        <textarea></textarea>
+      </div>
+    );
+  }
+
   renderAddButton() {
     return(
       <div className='add'>
@@ -54,6 +104,7 @@ class App extends React.Component{
       <div>
         <h1>Todo List</h1>
         <this.renderTodoItems />
+        <this.renderTextArea />
         <this.renderAddButton />
       </div>
     );
